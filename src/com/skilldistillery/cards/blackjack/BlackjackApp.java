470e6a890efeaaf132e6906bd38413fcf0215b01
parisasledge-source <parisasledge@me.com>
2022-03-13 03:06:28 -0400

    update3

--- src/com/skilldistillery/cards/blackjack/BlackjackApp.java
+++ src/com/skilldistillery/cards/blackjack/BlackjackApp.java
@@ -6,54 +6,59 @@ import java.util.Scanner;
 
 import com.skilldistillery.cards.common.Card;
 import com.skilldistillery.cards.common.Deck;
-//import com.skilldistillery.enums.solutions.cards.Card;
 
 public class BlackjackApp {
 
 	static Scanner sc = new Scanner(System.in);
-	
+
 	private BlackjackHand hand = new BlackjackHand();
 	private Deck deck = new Deck();
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		BlackjackApp app = new BlackjackApp();
-		
+
 		app.start();
-		
+
 		input.close();
 	}
 
 	private void start() {
 		Deck deck = new Deck();
 		deck.shuffle();
-		
+
 		hand.addCard(deck.dealCard());
-		
+
 		List<Card> hand = new ArrayList<>();
-		
+
 		String userInput = "";
-		do{
-			//System.out.println("h for hit, s for stand");
-			
-			//userInput = sc.next();
-			
-			//if (userInput == "h") {
-				
-				hand.add(deck.dealCard());
-				System.out.println(deck.dealCard());
-				System.out.println("This hand: " +  hand);
-			//}
-			
-			
-		}while (deck.checkDeckSize() > 10  );
+		//System.out.println("This hand: " + hand);
 		
-		for (int i = 0; i < 5; i++) {
-		}
+		do {
+
+			hand.add(deck.dealCard());
+			
+			System.out.println("This hand: " + hand);
 		
+			System.out.println("\nPlease enter 'h' for hit, 's' for stand: ");
+			userInput = sc.next();
+		} while (!userInput.equals("s"));
+		
+		do {
+			
+			hand.add(deck.dealCard());
+			System.out.println("This hand: " + hand);
+			
+			
+		
+		} while (deck.checkDeckSize() > 10);
+		
+		// while (deck.checkDeckSize() > 10 );
+
+
 		int handValue = 0;
 		for (Card dealtCard : hand) {
-			System.out.println(dealtCard + " (value:" + dealtCard.getValue() +")");
+			System.out.println(dealtCard + " (value:" + dealtCard.getValue() + ")");
 			handValue += dealtCard.getValue();
 		}
 		System.out.println("Hand value: " + handValue);
